f14b9f8358af6cd286413ea0847c123f59de4571
Cindon <Cynthia.Donch@gmail.com>
2023-02-25 16:58:17 -0500

    current issue with writing back to same pos

--- src/storagetest.py
+++ src/storagetest.py
@@ -1,44 +1,13 @@
 INT_BYTE_MAX_LEN = 7
 INT_BYTE_TYPE = "big"
 
-
-def bytes_to_int(byteRepresentation):
-    return int.from_bytes(byteRepresentation, INT_BYTE_TYPE)
-
-
-
-# with open("test2.bin", "wb+") as f:
-#     f.write(int.to_bytes(True))
-#     print(int.to_bytes(True))
-#     floatone=14
-#     f.write(floatone.to_bytes(INT_BYTE_MAX_LEN,INT_BYTE_TYPE))
-#     floattwo=7
-#     f.write(floattwo.to_bytes(INT_BYTE_MAX_LEN,INT_BYTE_TYPE))
-
-# with open("test2.bin", "rb") as f:
-#     value = bytes_to_int(f.read(1))
-#     match value:
-#         case 0:
-#             print("False")
-#         case 1:
-#             print("True")
-
-
-#     binfloatone = f.read(INT_BYTE_MAX_LEN)
-#     print(binfloatone)
-#     floatone = int.from_bytes(binfloatone, INT_BYTE_TYPE)
-#     print(floatone)
-#     binfloattwo = f.read(INT_BYTE_MAX_LEN)
-#     floattwo = int.from_bytes(binfloattwo, INT_BYTE_TYPE)
-#     value = float(str(floatone) + "." + str(floattwo))
-#     print(value)
-
 with open("test3.bin", "wb+") as f:
     f.write(int.to_bytes(1, INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
-    f.write(int.to_bytes(1, INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
-    f.write(int.to_bytes(190, INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
 
-with open("test3.bin", "rb") as f:
-    print(int.from_bytes(f.read(INT_BYTE_MAX_LEN), INT_BYTE_TYPE))
-    print(int.from_bytes(f.read(INT_BYTE_MAX_LEN), INT_BYTE_TYPE))
-    print(int.from_bytes(f.read(INT_BYTE_MAX_LEN), INT_BYTE_TYPE))
+with open("test3.bin", "wb+") as f:
+    int_to_increment = f.read(INT_BYTE_MAX_LEN)
+    incremented_int = int.from_bytes(int_to_increment, INT_BYTE_TYPE) + 1
+    print(int.from_bytes(int_to_increment, INT_BYTE_TYPE))
+    print(incremented_int)
+    #move f pointer back
+    f.write(int.to_bytes(incremented_int, INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
