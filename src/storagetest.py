ae486597716ffd2610fd27a79bec19a32407f719
Cindon <Cynthia.Donch@gmail.com>
2023-02-25 17:18:52 -0500

    f.seek

--- src/storagetest.py
+++ src/storagetest.py
@@ -2,12 +2,20 @@ INT_BYTE_MAX_LEN = 7
 INT_BYTE_TYPE = "big"
 
 with open("test3.bin", "wb+") as f:
-    f.write(int.to_bytes(1, INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
+    int_to_write = 20
+    print(int_to_write.to_bytes(INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
+    f.write(int_to_write.to_bytes(INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
 
-with open("test3.bin", "wb+") as f:
+with open("test3.bin", "rb+") as f:
     int_to_increment = f.read(INT_BYTE_MAX_LEN)
+    print(int_to_increment)
     incremented_int = int.from_bytes(int_to_increment, INT_BYTE_TYPE) + 1
     print(int.from_bytes(int_to_increment, INT_BYTE_TYPE))
     print(incremented_int)
     #move f pointer back
-    f.write(int.to_bytes(incremented_int, INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
+    neg = 0 - INT_BYTE_MAX_LEN
+    f.seek(-7,1)
+    print(incremented_int.to_bytes(INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
+    f.write(incremented_int.to_bytes(INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
+    test_int = f.read(INT_BYTE_MAX_LEN)
+    print(test_int)
