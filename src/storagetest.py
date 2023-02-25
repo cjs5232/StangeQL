5fde46221669acd6c267bae6466ea9028b5c9482
Cindon <Cynthia.Donch@gmail.com>
2023-02-25 18:06:20 -0500

    increment num pages properly

--- src/storagetest.py
+++ src/storagetest.py
@@ -7,6 +7,7 @@ with open("test3.bin", "wb+") as f:
     f.write(int_to_write.to_bytes(INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
 
 with open("test3.bin", "rb+") as f:
+    num_pages_pointer = f
     int_to_increment = f.read(INT_BYTE_MAX_LEN)
     print(int_to_increment)
     incremented_int = int.from_bytes(int_to_increment, INT_BYTE_TYPE) + 1
@@ -16,6 +17,9 @@ with open("test3.bin", "rb+") as f:
     neg = 0 - INT_BYTE_MAX_LEN
     f.seek(neg,1)
     print(incremented_int.to_bytes(INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
-    f.write(incremented_int.to_bytes(INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
-    test_int = f.read(INT_BYTE_MAX_LEN)
-    print(test_int)
+    num_pages_pointer.write(incremented_int.to_bytes(INT_BYTE_MAX_LEN, INT_BYTE_TYPE))
+
+with open("test3.bin", "rb+") as f:
+    incremented_int = f.read(INT_BYTE_MAX_LEN)
+    incremented_int = int.from_bytes(incremented_int, INT_BYTE_TYPE)
+    print(incremented_int)
