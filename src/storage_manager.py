383762650f7a9503974b1f4380fee29132e4e1a2
Cindon <Cynthia.Donch@gmail.com>
2023-02-25 19:35:41 -0500

    removing get record by primary key

--- src/storage_manager.py
+++ src/storage_manager.py
@@ -33,41 +33,15 @@ class StorageManager:
         #if there is not a table yet
         if os.path.exists(filepath):
             print(f"Table {table_name} already exists")
-            return 0
+            return 1
         #create a new table
         f = open(filepath, "wb+")
-        #TODO write 0 as an integer to the table, there are currently 0 pages.
-        f.write(int.to_bytes(0, self.INT_BYTE_MAX_LEN, self.INT_BYTE_TYPE))
-        return 1
-
-    # phase 1
-    def get_record(self, primary_key):
-        """
-        getCatTables = self.get_catalog(self)
-        tables = getCatTables["tables"]
-        for table in tables:
-            filepath = self.dbloc + "/" + table["name"] + ".bin"
-            data_types = []
-            for i in range(len(table["attributes"])):
-                attribute = table["attributes"][i]
-                if attribute[primary_key]:
-                    primary_index = i
-                data_types[i] = self.get_dtype(attribute["type"])
-
-            f = open(filepath, "rb")
-            table_file = f.read()
-            #splitting the file by pages
-            table_file = f.split("</page>")
-            num_pages = int(table_file[1])
-            #going through each page in the table_file
-            for i in range(num_pages) - 1:
-                records = table_file[i].split("</record>")
-                for j in range(len(records)):
-                    record = records[j].split(",")
-                    if(primary_key == record[primary_index]):
-                        return tuple(record)
+        #write 0 as an integer to the table, there are currently 0 pages.
+        #f.write(int.to_bytes(0, self.INT_BYTE_MAX_LEN, self.INT_BYTE_TYPE))
         return 0
-        """
+
+    def get_record(self, primary_key):
+
         return
 
     def get_page(self, table_number, page_number):
@@ -79,10 +53,8 @@ class StorageManager:
     #phase 1
     #returns a list of tuples ex: [(), (), ()][(), (), ()]
     def get_records(self, table_name):
+        #get table attributes from catalog
         attributes = self.cat.table_attributes(table_name)
-
-        #get types of each attribute
-        data_types = []
         records = []
         #TODO if filepath does not exist, throw an error
         filepath = self.dbloc + "/" + table_name + ".bin"
@@ -102,13 +74,18 @@ class StorageManager:
                         print(attribute_type)
                         if attribute_type == 'integer':
                             value = self.bytes_to_int(f.read(self.INT_BYTE_MAX_LEN))
-                        #if bool
-                            #value = self.bytes_to_int(f.read(1))
-                            #match value
-                                #case 0:
-                                    #record.append("False")
-                                #case 1:
-                                    #record.append("True")
+                        elif attribute_type == 'boolean':
+                            value = self.bytes_to_int(f.read(1))
+                            if value == 0:
+                                record.append("False")
+                            elif value == 1:
+                                record.append("True")
+                        elif attribute_type == 'double':
+                            binfloatone = f.read(self.INT_BYTE_MAX_LEN)
+                            floatone = int.from_bytes(binfloatone, self.INT_BYTE_TYPE)
+                            binfloattwo = f.read(self.INT_BYTE_MAX_LEN)
+                            floattwo = int.from_bytes(binfloattwo, self.INT_BYTE_TYPE)
+                            value = float(str(floatone) + "." + str(floattwo))
                         #if fixed len char
                             #char_len = TODO get the length of the char in bytes
                                 #this can be gotten from the attribute table from the catalog
@@ -120,14 +97,7 @@ class StorageManager:
                             #print(knownInt)
                             #knownString = f.read(knownInt)
                             #print(knownString.decode())
-                        #if double
-                            #binfloatone = f.read(self.INT_BYTE_MAX_LEN)
-                            #floatone = int.from_bytes(binfloatone, self.INT_BYTE_TYPE)
-                            #binfloattwo = f.read(self.INT_BYTE_MAX_LEN)
-                            #floattwo = int.from_bytes(binfloattwo, self.INT_BYTE_TYPE)
-                            #value = float(str(floatone) + "." + str(floattwo))
-                            #
-                        
+
                         #attribute_len = get_len()
                         #record.append(get_len())
 
@@ -135,7 +105,6 @@ class StorageManager:
                         record.append(value)
                     records.append(record)
             return records
-        return 1
 
     #helper function to get the length to read
     #attribute is the string of the current attribute being checked: ex)
@@ -143,8 +112,8 @@ class StorageManager:
         return
 
     # phase 1
-    #attributes is a dictionary holding attributes of each in the form (name, type) if key=primarykey value=column name
     def insert_record(self, table_name, attributes, values):
+        print(len(values))
         if (len(attributes) != len(values)):
             print("SM: Attribute size does not equal value size")
             return 0
@@ -164,30 +133,30 @@ class StorageManager:
             #if the attribute is the primary key, store the primary index for later
             if attribute["primary_key"]:
                 primary_index = i
-            #add this attribute's data type using a helper function that parses the attribute type from the catalog
-            ####attribute_type = self.get_dtype(attribute["type"])
-            #append this to the data types list
-            ####data_types.append(attribute_type)
         #open the table file to read and write in binary
         with open(filepath, "rb+") as f:
+            num_pages_pointer = f
             #read in the number of pages
             num_pages = self.bytes_to_int(f.read(self.INT_BYTE_MAX_LEN))
             #if there are 0 pages, create a new page
             if num_pages == 0:
-                #increment catalog page count
-                self.cat.update_page_count(table_name,1)
-                #write page count to the table
-                f.write(int.to_bytes(1, self.INT_BYTE_MAX_LEN, self.INT_BYTE_TYPE))
                 #increment num_pages to 1
                 num_pages = 1
-            
+                #increment catalog page count
+                self.cat.update_page_count(table_name,1)
+                print(int.to_bytes(num_pages, self.INT_BYTE_MAX_LEN, self.INT_BYTE_TYPE))
+                #write page count to the table
+                num_pages_pointer.write(int.to_bytes(num_pages, self.INT_BYTE_MAX_LEN, self.INT_BYTE_TYPE))
+                
             #for each page, read in the number of records, then read each record
             for i in range(num_pages):
+                num_records_pointer = f
                 num_records = self.bytes_to_int(f.read(self.INT_BYTE_MAX_LEN))
                 #if there are 0 records, write the value.
                 if num_records == 0:
+                    num_records+=1
                     #write the number of records to the page
-                    f.write(int.to_bytes(1, self.INT_BYTE_MAX_LEN, self.INT_BYTE_TYPE))
+                    num_records_pointer.write(int.to_bytes(1, self.INT_BYTE_MAX_LEN, self.INT_BYTE_TYPE))
                     print(values)
                     print(table_attributes)
                     #write a record to this position
@@ -196,8 +165,8 @@ class StorageManager:
                     #return successfully wrote record
                     return 0
                 #for each record in the page, read each attribute
-                #for j in range(num_records):
-                 #   record = []
+                for j in range(num_records):
+                    record = []
                     #for each attribute in the record
             #once the end of the file is reached, append new record
             
